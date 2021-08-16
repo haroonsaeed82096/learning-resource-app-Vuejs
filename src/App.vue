3e17062675f3e0ab28b580e7d239fc98210fb5cc
haroonsaeed82096 <30671721+haroonsaeed82096@users.noreply.github.com>
2021-08-16 23:26:21 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'offcial-guide',
+          title: 'official guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'Google',
+          title: 'Google',
+          description: 'Learn to google.....',
+          link: 'https://google.org'
+        }
+      ]
+    };
+  }
+};
+</script>
